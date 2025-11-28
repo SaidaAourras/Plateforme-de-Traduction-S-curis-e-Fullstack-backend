d1087c1f9f366a8ee78a325cdb1727c51ea3ce4a
SaidaAourras <sa123aouras@gmail.com>
2025-11-28 21:34:27 +0100

    modifier type query params dans endpoint translate

--- api/v1/routes/translate.py
+++ api/v1/routes/translate.py
@@ -1,10 +1,11 @@
 from fastapi import  APIRouter , Depends
 from services.translate_services import translate_text
 from services.auth_service import verify_token
+from ..schemas.translate import TranslateRequest
 
 translate_router = APIRouter(prefix="/translate" , tags=["translation"])
 
-@translate_router.post('/{choice}')
-def translate(choice: str , text: str, user= Depends(verify_token)):
+@translate_router.post('/')
+def translate(request:TranslateRequest, user= Depends(verify_token)):
     
-    return {"user": user.username, "translated": translate_text(choice, text)}
+    return {"user": user.username, "translated": translate_text(request.choice, request.text)}
