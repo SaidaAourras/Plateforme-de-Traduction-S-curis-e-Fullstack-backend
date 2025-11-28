9e3b66d7921467184d79bed0e66e05b55003b90e
SaidaAourras <sa123aouras@gmail.com>
2025-11-28 21:38:47 +0100

    configuration du cors

---
+++ core/config.py
@@ -0,0 +1,17 @@
+# from pydantic_settings import BaseSettings
+
+# class Settings(BaseSettings):
+    
+#     BACKEND_CORS_ORIGINS: list[str] = [
+#         "http://localhost:3000"
+#     ]
+    
+#     HF_TOKEN : str
+#     DATABASE : str
+#     ALGORITHM : str
+#     SECRET_KEY : str
+    
+#     class Config:
+#         env_file = ".env"
+        
+# settings = Settings()
